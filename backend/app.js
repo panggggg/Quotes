f9d615af476ae6196ce3ee5e69a6db53fa8520c7
panggggg <p.picczy@gmail.com>
2021-09-23 22:55:26 +0700

    Add app.use()

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,12 @@ const Quote = require('inspirational-quotes')
 
 const app = express()
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    next()
+})
+
 app.get('/', (req, res) => {
     res.send(Quote.getQuote())
 })
